816eb6195fa3f440f2aa8fd2356b05b383e3c826
HUANG-YU-CHENG <ve8132966@gmail.com>
2024-06-18 21:31:56 +0800

    新增Footer.vue、更新html檔、更新App.vue檔

---
+++ JS_final_exam_project/personal_blog/src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-gray-100 flex flex-col">
+        <footer class="bg-gray-100 text-center py-4 shadow-md">
+            <p class="text-gray-500">&copy; 2024 黃昱誠的個人部落格</p>
+        </footer>
+    </div>
+</template>
